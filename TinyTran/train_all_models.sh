cf8efff4995ee35644d9832ce42c33fbbf37839b
Damir Wallener <damir00@gmail.com>
2025-05-19 14:49:26 -0700

    sample training script

--- TinyTran/train_all_models.sh
+++ TinyTran/train_all_models.sh
@@ -3,24 +3,80 @@
 # cello, violo, violin1, violin2 (melody)
 
 TRAIN_SCRIPT="train_phrase_transformer.py"
-PHRASE_TYPES=("short" "medium" "long")
 
-for TYPE in "${PHRASE_TYPES[@]}"; do
-    DATASET="cello_phrase_dataset_${TYPE}.json"
-    MODEL_OUTPUT="tiny_transformer_cello_${TYPE}.pt"
-    VOCAB_OUTPUT="tiny_transformer_cello_${TYPE}.vocab.json"
+INSTRUMENT="cello"
 
-    # Capitalize first letter manually for display
-    if [ "$TYPE" = "short" ]; then
-        TYPE_CAP="Short"
-    elif [ "$TYPE" = "medium" ]; then
-        TYPE_CAP="Medium"
-    else
-        TYPE_CAP="Long"
-    fi
+DATASET="${INSTRUMENT}_phrase_dataset_short.json"
+MODEL_OUTPUT="Cello_Short.model.pt"
+VOCAB_OUTPUT="Cello_Short.vocab.json"
+python3 $TRAIN_SCRIPT $DATASET --model_out $MODEL_OUTPUT --vocab_out $VOCAB_OUTPUT
 
-    echo "Training Cello ${TYPE_CAP} Model..."
-    python3 $TRAIN_SCRIPT $DATASET --model_out $MODEL_OUTPUT --vocab_out $VOCAB_OUTPUT
-done
+DATASET="${INSTRUMENT}_phrase_dataset_medium.json"
+MODEL_OUTPUT="Cello_Medium.model.pt"
+VOCAB_OUTPUT="Cello_Medium.vocab.json"
+python3 $TRAIN_SCRIPT $DATASET --model_out $MODEL_OUTPUT --vocab_out $VOCAB_OUTPUT
 
-echo "All cello models trained successfully."
+DATASET="${INSTRUMENT}_phrase_dataset_long.json"
+MODEL_OUTPUT="Cello_Long.model.pt"
+VOCAB_OUTPUT="Cello_Long.vocab.json"
+python3 $TRAIN_SCRIPT $DATASET --model_out $MODEL_OUTPUT --vocab_out $VOCAB_OUTPUT
+
+echo "All ${INSTRUMENT} models trained successfully."
+
+INSTRUMENT="viola"
+
+DATASET="${INSTRUMENT}_phrase_dataset_short.json"
+MODEL_OUTPUT="Viola_Short.model.pt"
+VOCAB_OUTPUT="Viola_Short.vocab.json"
+python3 $TRAIN_SCRIPT $DATASET --model_out $MODEL_OUTPUT --vocab_out $VOCAB_OUTPUT
+
+DATASET="${INSTRUMENT}_phrase_dataset_medium.json"
+MODEL_OUTPUT="Viola_Medium.model.pt"
+VOCAB_OUTPUT="Viola_Medium.vocab.json"
+python3 $TRAIN_SCRIPT $DATASET --model_out $MODEL_OUTPUT --vocab_out $VOCAB_OUTPUT
+
+DATASET="${INSTRUMENT}_phrase_dataset_long.json"
+MODEL_OUTPUT="Viola_Long.model.pt"
+VOCAB_OUTPUT="Viola_Long.vocab.json"
+python3 $TRAIN_SCRIPT $DATASET --model_out $MODEL_OUTPUT --vocab_out $VOCAB_OUTPUT
+
+echo "All ${INSTRUMENT} models trained successfully."
+
+INSTRUMENT="violin1"
+
+DATASET="${INSTRUMENT}_phrase_dataset_short.json"
+MODEL_OUTPUT="Violin1_Short.model.pt"
+VOCAB_OUTPUT="Violin1_Short.vocab.json"
+python3 $TRAIN_SCRIPT $DATASET --model_out $MODEL_OUTPUT --vocab_out $VOCAB_OUTPUT
+
+DATASET="${INSTRUMENT}_phrase_dataset_medium.json"
+MODEL_OUTPUT="Violin1_Medium.model.pt"
+VOCAB_OUTPUT="Violin1_Medium.vocab.json"
+python3 $TRAIN_SCRIPT $DATASET --model_out $MODEL_OUTPUT --vocab_out $VOCAB_OUTPUT
+
+DATASET="${INSTRUMENT}_phrase_dataset_long.json"
+MODEL_OUTPUT="Violin1_Long.model.pt"
+VOCAB_OUTPUT="Violin1_Long.vocab.json"
+python3 $TRAIN_SCRIPT $DATASET --model_out $MODEL_OUTPUT --vocab_out $VOCAB_OUTPUT
+
+echo "All ${INSTRUMENT} models trained successfully."
+
+INSTRUMENT="violin2"
+
+DATASET="${INSTRUMENT}_phrase_dataset_short.json"
+MODEL_OUTPUT="Violin2_Short.model.pt"
+VOCAB_OUTPUT="Violin2_Short.vocab.json"
+python3 $TRAIN_SCRIPT $DATASET --model_out $MODEL_OUTPUT --vocab_out $VOCAB_OUTPUT
+
+DATASET="${INSTRUMENT}_phrase_dataset_medium.json"
+MODEL_OUTPUT="Violin2_Medium.model.pt"
+VOCAB_OUTPUT="Violin2_Medium.vocab.json"
+python3 $TRAIN_SCRIPT $DATASET --model_out $MODEL_OUTPUT --vocab_out $VOCAB_OUTPUT
+
+DATASET="${INSTRUMENT}_phrase_dataset_long.json"
+MODEL_OUTPUT="Violin2_Long.model.pt"
+VOCAB_OUTPUT="Violin2_Long.vocab.json"
+python3 $TRAIN_SCRIPT $DATASET --model_out $MODEL_OUTPUT --vocab_out $VOCAB_OUTPUT
+
+echo "All ${INSTRUMENT} models trained successfully."
+
